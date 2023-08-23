b3633d251df9b2ee5e36bbbd568d9bf047a15a24
junaid-repo <junaidraza3001@gmail.com>
2023-08-23 16:34:11 +0530

    splitwise

--- sgroups/src/main/java/com/splitwise/app/sgroups/controller/SGroupController.java
+++ sgroups/src/main/java/com/splitwise/app/sgroups/controller/SGroupController.java
@@ -4,6 +4,7 @@ import com.splitwise.app.sgroups.dto.SaveGroupResponse;
 import com.splitwise.app.sgroups.dto.GroupDetails;
 import com.splitwise.app.sgroups.entites.GroupMembers;
 import com.splitwise.app.sgroups.service.GroupServices;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ public class SGroupController {
     }
 
     @PostMapping("/createGroup")
+    @CircuitBreaker(name = "createGroupCB", fallbackMethod = "fallbackMethodForCreateGroup")
     ResponseEntity<SaveGroupResponse> createGroup(@RequestBody GroupDetails request) {
 
         SaveGroupResponse response = new SaveGroupResponse();
@@ -35,14 +37,22 @@ public class SGroupController {
 
 
     }
-    @GetMapping("/getGroupMembers/{groupName}")
-    ResponseEntity<List<GroupMembers>> getGroupDetails(@PathVariable String groupName){
 
-        List<GroupMembers> response= new ArrayList<>();
-        response=serv.getGroupDetails(groupName);
+    @GetMapping("/getGroupMembers/{groupName}")
+    ResponseEntity<List<GroupMembers>> getGroupDetails(@PathVariable String groupName) {
+
+        List<GroupMembers> response = new ArrayList<>();
+        response = serv.getGroupDetails(groupName);
 
         return ResponseEntity.status(HttpStatus.FOUND).body(response);
 
     }
 
+   public ResponseEntity<SaveGroupResponse> fallbackMethodForCreateGroup(GroupDetails request, Exception ex) {
+        SaveGroupResponse fmr = new SaveGroupResponse();
+        ex.printStackTrace();
+        fmr.setReturnMsg("The calling service is not active so breaking the flow");
+        fmr.setReturnCode("503");
+        return new ResponseEntity<>(fmr,HttpStatus.BAD_GATEWAY);
+    }
 }
