374c2505d0b36ee36bc1e9142d2b0244e25a557e
junaid-repo <junaidraza3001@gmail.com>
2023-08-24 13:54:26 +0530

    Logs and settleBalance service

---
+++ sbills/src/main/java/com/splitwise/app/sbills/dto/UserLogResponse.java
@@ -0,0 +1,16 @@
+package com.splitwise.app.sbills.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserLogResponse {
+	List<LogMessageDetails> logs;
+	
+
+}
