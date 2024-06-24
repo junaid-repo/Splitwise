65bb1134784b7bbc47b2e28cf14a9a45f89d6da8
junaid-repo <95649150+junaid-repo@users.noreply.github.com>
2024-06-24 20:01:57 +0530

    adding docker files

--- sbills/src/main/java/com/splitwise/app/sbills/SbillsApplication.java
+++ sbills/src/main/java/com/splitwise/app/sbills/SbillsApplication.java
@@ -1,7 +1,11 @@
 package com.splitwise.app.sbills;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -17,8 +21,8 @@ public class SbillsApplication {
 
 	@Bean
 	@LoadBalanced
-	public RestTemplate restTemplate(){
-		return  new RestTemplate();
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
