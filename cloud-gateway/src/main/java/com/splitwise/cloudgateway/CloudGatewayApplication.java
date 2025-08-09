2a94a19453fa06a3e5c4e3c6ff689fbbaedcf02f
junaid-repo <junaidraza3001@gmail.com>
2025-08-09 18:36:42 +0530

    adding updates for aws integrations

--- cloud-gateway/src/main/java/com/splitwise/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/splitwise/cloudgateway/CloudGatewayApplication.java
@@ -3,14 +3,23 @@ package com.splitwise.cloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@GetMapping("/healtTest")
+	ResponseEntity<String> healthTest() {
+		return ResponseEntity.ok("Cloud Gateway is running");
+
+	}
 }
 
--- config-server/src/main/java/com/splitwise/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/splitwise/configserver/ConfigServerApplication.java
@@ -3,13 +3,21 @@ package com.splitwise.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
+@RestController
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
+	@GetMapping("/healtTest")
+	ResponseEntity<String> healthTest() {
+		return ResponseEntity.ok("Config Server is running");
 
+	}
 }
